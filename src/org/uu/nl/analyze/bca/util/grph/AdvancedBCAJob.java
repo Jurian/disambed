14b938815082f3cb4482e5ec1b83d68d364a0aac
Jurian <j.baas@uu.nl>
2018-12-14 18:22:33 +0100

    Untest version of BCA using Grph library

--- src/org/uu/nl/analyze/bca/util/grph/AdvancedBCAJob.java
+++ src/org/uu/nl/analyze/bca/util/grph/AdvancedBCAJob.java
@@ -2,7 +2,6 @@ package org.uu.nl.analyze.bca.util.grph;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import org.uu.nl.analyze.bca.util.BCV;
@@ -10,15 +9,20 @@ import org.uu.nl.analyze.bca.util.PaintRegistry;
 import org.uu.nl.convert.Rdf2GrphConverter;
 
 import grph.Grph;
-import toools.collections.primitive.LucIntSet;
+import grph.properties.NumericalProperty;
 
 public class AdvancedBCAJob extends BCAJob {
 
 	private final Map<Integer, BCV> computedBCV;
+	private final int[][] in, out;
 	
-	public AdvancedBCAJob(Map<Integer, BCV> computedBCV, int bookmark, boolean reverse, boolean normalize, double alpha, double epsilon, Grph graph) {
+	public AdvancedBCAJob(Map<Integer, BCV> computedBCV, 
+			int bookmark, boolean reverse, boolean normalize, double alpha, double epsilon, 
+			Grph graph, int[][] in, int[][] out) {
 		super(bookmark, reverse, normalize, alpha, epsilon, graph);
 		this.computedBCV = computedBCV;
+		this.in = in;
+		this.out = out;
 	}
 
 	@Override
@@ -32,17 +36,19 @@ public class AdvancedBCAJob extends BCAJob {
 		final Map<Integer, Integer> usePredicate = new HashMap<>();
 		final Map<Integer, Integer> ignoreNeighbour = new HashMap<>();
 		
+		final NumericalProperty types = graph.getEdgeColorProperty();
+		
 		nodeQueue.add(bookmark);
 		wetPaintRegister.put(bookmark, 1d);
 		
 		while (!nodeQueue.isEmpty()) {
 
-			final Integer focusNode = nodeQueue.poll();
+			final int focusNode = nodeQueue.poll();
 			final double wetPaint = wetPaintRegister.get(focusNode);
 
 			// Keep part of the available paint on this node, distribute the rest
 			//final int focusNodeIndex = keys.get(focusNode);
-			System.out.println("Focus: " + focusNode.toString() );
+			System.out.println("Focus: " + focusNode);
 			
 			final BCV precomputed = computedBCV.get(focusNode);
 			if(precomputed != null) {
@@ -58,48 +64,75 @@ public class AdvancedBCAJob extends BCAJob {
 				if (wetPaint < epsilon)
 					continue;
 
-				
-				final LucIntSet neighbours, predicates;
-				final int ignored = ignoreNeighbour.getOrDefault(focusNode, -1);
-				
+				//The set of vertices adjacent to vertex v are found in the array in[v], out[v]
+				final int ignoredIndex = ignoreNeighbour.getOrDefault(focusNode, -1);
+				final int[] neighbors;
+				final int[] edges;
+				final boolean[] ignored;
+				int neighborCount;
+	
 				// We do something different in case of a literal node
-				if(graph.getEdgeColorProperty().getValue(focusNode) == Rdf2GrphConverter.LITERAL) {
+				if(types.getValue(focusNode) == Rdf2GrphConverter.LITERAL) {
 					
-					int[] edges = graph.getInEdges(focusNode).toIntArray();
-					int predicate = usePredicate.get(focusNode);
-					neighbours = graph.getInNeighbors(focusNode);
-					neighbours.removeIf(i -> edges[i] != predicate || i == ignored);
+					final int predicate = usePredicate.get(focusNode);
+					
+					neighbors = in[focusNode];
+					neighborCount = neighbors.length;
+					ignored = new boolean[neighbors.length];
+					edges = graph.getInEdges(focusNode).toIntArray();
+					
+					for(int i = 0; i < neighbors.length; i++) {
+						if(neighbors[i] == ignoredIndex || edges[i] == predicate) {
+							ignored[i] = true;
+							neighborCount--;
+						}
+					}
+
 					// Remove this node from the map in case we reach it again from a different predicate
 					System.out.println("Came in from " + usePredicate.get(focusNode) + " and " + ignoreNeighbour.get(focusNode));
 					usePredicate.remove(focusNode);
 				} else {
-					neighbours = graph.getOutNeighbors(focusNode);
-					neighbours.removeIf(i -> i == ignored);
+					
+					neighbors = out[focusNode];
+					neighborCount = neighbors.length;
+					ignored = new boolean[neighbors.length];
+					edges = graph.getOutEdges(focusNode).toIntArray();
+					
+					for(int i = 0; i < neighbors.length; i++) {
+						if(neighbors[i] == ignoredIndex) {
+							ignored[i] = true;
+							neighborCount--;
+						}
+					}
 				}
 				
-				final long neighbourCount = neighbours.size();
+				
 				System.out.println("Neighbours:");
-				for (int i : neighbours) {
+				for (int i = 0; i < neighbors.length; i++) {
+					
+					if(ignored[i]) continue;
+					
+					final int neighbor = neighbors[i];
+					
 					System.out.println(i);
 					
-					final int predicate = t.getPredicate();
-					final int predicateIndex = keys.get(predicate);
-					final double weight = 1 / (double) neighbourCount;
+					final int predicate = edges[i];
+					final double weight = 1 / (double) neighborCount;
 					final double partialWetPaint = (1 - alpha) * wetPaint * weight;
 
-					bcv.add(predicateIndex, partialWetPaint);
+					bcv.add(predicate, partialWetPaint);
 					
-					if (nodeQueue.contains(neighbour)) {
-						wetPaintRegister.add(neighbour, partialWetPaint);
+					if (nodeQueue.contains(neighbor)) {
+						wetPaintRegister.add(neighbor, partialWetPaint);
 					} else {
-						nodeQueue.add(neighbour);
-						wetPaintRegister.put(neighbour, partialWetPaint);
+						nodeQueue.add(neighbor);
+						wetPaintRegister.put(neighbor, partialWetPaint);
 					}
 					
 					// When we consider this literal neighbor later we have to remember with predicate we used
 					// This way we don't visit nodes that have a different relationship with the literal
-					if(neighbour.isLiteral() || focusNode.isLiteral()) ignoreNeighbour.put(neighbour, focusNode);
-					if(neighbour.isLiteral()) usePredicate.put(neighbour, predicate);
+					if(types.getValue(neighbor) == Rdf2GrphConverter.LITERAL || types.getValue(focusNode) == Rdf2GrphConverter.LITERAL) ignoreNeighbour.put(neighbor, focusNode);
+					if(types.getValue(neighbor) == Rdf2GrphConverter.LITERAL) usePredicate.put(neighbor, predicate);
 				}
 				System.out.println();
 			}
