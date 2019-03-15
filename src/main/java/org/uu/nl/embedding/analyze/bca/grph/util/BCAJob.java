eb75ad7e91eed92f9cdb6c1c7b892f003e30781a
Jurian <j.baas@uu.nl>
2019-03-15 17:35:53 +0100

    Moved edge finding method to this class

--- src/main/java/org/uu/nl/embedding/analyze/bca/grph/util/BCAJob.java
+++ src/main/java/org/uu/nl/embedding/analyze/bca/grph/util/BCAJob.java
@@ -13,17 +13,16 @@ import java.util.concurrent.Callable;
 public abstract class BCAJob implements Callable<BCV> {
 
 	protected final int bookmark;
-	protected final boolean reverse, normalize;
+	private final boolean reverse;
 	protected final double alpha, epsilon;
 	protected final Grph graph;
 
-	public BCAJob(
-			int bookmark, boolean reverse, boolean normalize,
-			double alpha, double epsilon, 
-			Grph graph) {
+	protected BCAJob(
+            int bookmark, boolean reverse,
+            double alpha, double epsilon,
+            Grph graph) {
 
 		this.reverse = reverse;
-		this.normalize = normalize;
 		this.bookmark = bookmark;
 		this.alpha = alpha;
 		this.epsilon = epsilon;
@@ -40,4 +39,19 @@ public abstract class BCAJob implements Callable<BCV> {
 	}
 
 	protected abstract BCV doWork(Grph graph, boolean reverse);
+
+	protected int getEdge(int source, int destination, int[] out, int[] in) {
+		if (out.length == 0 || in.length == 0) {
+			return -1;
+		} else {
+			if (out.length < in.length) {
+				for(int e : out)
+					if (graph.getDirectedSimpleEdgeHead(e) == destination) return e;
+			} else {
+				for(int e : in)
+					if (graph.getDirectedSimpleEdgeTail(e) == source) return e;
+			}
+			return -1;
+		}
+	}
 }
