16c8d2599596b6ee6bb70bfec43d72e3d66bdc2a
Jurian <j.baas@uu.nl>
2019-03-20 13:26:04 +0100

    Number of vertices in the RDF graph is now calculated correctly

--- src/main/java/org/uu/nl/embedding/convert/Rdf2GrphConverter.java
+++ src/main/java/org/uu/nl/embedding/convert/Rdf2GrphConverter.java
@@ -9,104 +9,127 @@ import org.apache.jena.graph.Triple;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.util.iterator.ExtendedIterator;
+import org.apache.log4j.Logger;
 import org.uu.nl.embedding.convert.util.NodeInfo;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Rdf2GrphConverter implements Converter<Grph, Model> {
-	
+
+	final static Logger logger = Logger.getLogger(Rdf2GrphConverter.class);
+
 	private static int type2color(Node node) {
 		if(node.isURI()) return NodeInfo.URI;
 		else if (node.isBlank()) return NodeInfo.BLANK;
 		else if (node.isLiteral()) return NodeInfo.LITERAL;
 		else throw new IllegalArgumentException("Node " + node + " is not of type URI, blank or literal");
 	}
-	
-	private final boolean literals;
-	
-	public Rdf2GrphConverter() {
-		this(true);
-	}
-	
-	public Rdf2GrphConverter(boolean literals) {
-		this.literals = literals;
-	}
-	
+
 	@Override
 	public Grph convert(Model model) {
 		
 		final Grph g = new InMemoryGrph();
-		final NumericalProperty vertexType = g.getVertexColorProperty();
+
 		final NumericalProperty edgeType = g.getEdgeColorProperty();
-		final Property vertexLabel = g.getVertexLabelProperty();
+
 		final Property edgeLabel = g.getEdgeLabelProperty();
 		
-		final Map<Node, Integer> vertexMap = new HashMap<>();
+		final Map<Node, Integer> vertexMap = getVertices(model, g);
 		final Map<Node, Integer> edgeMap = new HashMap<>();
-		
-		int s_i, o_i, p_i = getVertexCount(model);
+
+		final int vertexCount = vertexMap.size();
+
+		logger.info("Converting Jena model with "+vertexCount+" vertices");
+
+		int s_i, o_i, p_i = 0;
 		Node s, p, o;
 		Triple t;
 		
-		ExtendedIterator<Triple> triples = model.getGraph().find();
-		
-		while(triples.hasNext()) {
-			t = triples.next();
-			s = t.getSubject();
-			p = t.getPredicate();
-			o = t.getObject();
-			
-			// Skip if we don't want literals
-			if(!literals && o.isLiteral()) continue;
+		final ExtendedIterator<Triple> triples = model.getGraph().find();
 
-			// Only create a new ID if the subject is not yet present
-			if(vertexMap.containsKey(s)) s_i = vertexMap.get(s);
-			else s_i = addVertex(g, s, vertexMap, vertexType, vertexLabel);
+		try {
+			while (triples.hasNext()) {
+				t = triples.next();
+				s = t.getSubject();
+				p = t.getPredicate();
+				o = t.getObject();
 
-			// Only create a new ID if the object is not yet present
-			if(vertexMap.containsKey(o)) o_i = vertexMap.get(o);
-			else o_i = addVertex(g, o, vertexMap, vertexType, vertexLabel);
+				assert vertexMap.containsKey(s);
+				assert vertexMap.containsKey(o);
 
-			// Every edge is unique, we always add one to the graph
-			// However there are only a few types of edges (relationships)
-			// So we also store a reference to a unique edge-type id
-			g.addDirectedSimpleEdge(s_i, p_i, o_i);
-			edgeLabel.setValue(p_i, p.toString());
+				s_i = vertexMap.get(s);
+				o_i = vertexMap.get(o);
 
-			// If we have not encountered this edge-type before, give it a unique id
-			edgeMap.putIfAbsent(p, edgeMap.size());
-			// Store the edge-type value for this new edge
-			edgeType.setValue(p_i, edgeMap.get(p));
-			p_i++;
+				// Every edge is unique, we always add one to the graph
+				// However there are only a few types of edges (relationships)
+				// So we also store a reference to a unique edge-type id
+				g.addDirectedSimpleEdge(s_i, p_i + vertexCount, o_i);
+				edgeLabel.setValue(p_i + vertexCount, p.toString());
+
+				// If we have not encountered this edge-type before, give it a unique id
+				edgeMap.putIfAbsent(p, edgeMap.size());
+				// Store the edge-type value for this new edge
+				edgeType.setValue(p_i + vertexCount, edgeMap.get(p));
+				p_i++;
+			}
+		} finally {
+			triples.close();
 		}
+
+		assert g.getVertices().size() == vertexCount: g.getVertices().size() + "!=" + vertexCount;
+
 		return g;
 	}
 
-	private int addVertex(Grph g, Node n, Map<Node, Integer> vertexMap, NumericalProperty vertexType, Property vertexLabel) {
-		int i = vertexMap.size();
-		g.addVertex(i);
-		vertexType.setValue(i, type2color(n));
-		vertexLabel.setValue(i, n.toString());
-		vertexMap.put(n, i);
+	private Map<Node, Integer> getVertices(Model model, Grph g) {
 
+		final Map<Node, Integer> vertexMap = new HashMap<>();
+		final ExtendedIterator<Triple> triples = model.getGraph().find();
+
+		try {
+			Node s, o;
+			Triple t;
+
+			while (triples.hasNext()) {
+				t = triples.next();
+				s = t.getSubject();
+				o = t.getObject();
+
+				// Only create a new ID if the subject is not yet present
+				if (!vertexMap.containsKey(s)) addVertex(g, s, vertexMap);
+				if (!vertexMap.containsKey(o)) addVertex(g, o, vertexMap);
+			}
+		} finally {
+			triples.close();
+		}
+
+		return vertexMap;
+	}
+
+	private int addVertex(Grph g, Node n, Map<Node, Integer> vertexMap) {
+		final int i = vertexMap.size();
+		g.addVertex(i);
+		g.getVertexColorProperty().setValue(i, type2color(n));
+		g.getVertexLabelProperty().setValue(i, n.toString());
+		vertexMap.put(n, i);
 		return i;
 	}
 
+	/*
 	private int getVertexCount(Model model) {
-		String sparql = "SELECT (COUNT(DISTINCT ?vertex) AS ?vertexCount) " + 
+		final String sparql = "SELECT (COUNT(DISTINCT ?vertex) AS ?vertexCount) " +
 				"WHERE" + 
 				"{" + 
 					"{" + 
 						"?vertex ?p [] " + 
 					"}" + 
-					"UNION" + 
+					"UNION" +
 					"{ " + 
 						"[] ?p ?vertex " + 
 						"FILTER(!IsLiteral(?vertex))" + 
 					"}" + 
 				"}";
-
 		Query qry = QueryFactory.create(sparql);
 		try(QueryExecution qe = QueryExecutionFactory.create(qry, model)) {
 			ResultSet rs = qe.execSelect();
@@ -117,5 +140,5 @@ public class Rdf2GrphConverter implements Converter<Grph, Model> {
 		}
 		return -1;
 
-	}
+	}*/
 }
