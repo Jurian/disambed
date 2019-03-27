6231c51c2e08f102360f791cae015ab7c9d07678
Jurian <j.baas@uu.nl>
2019-03-27 14:51:14 +0100

    Fixed warnings in code, added javadoc and class is now able to output a projection matrix

--- src/main/java/org/uu/nl/embedding/pca/PCA.java
+++ src/main/java/org/uu/nl/embedding/pca/PCA.java
@@ -3,7 +3,6 @@ package org.uu.nl.embedding.pca;
 import com.github.fommil.netlib.LAPACK;
 
 import org.apache.commons.math.util.FastMath;
-import org.apache.log4j.Logger;
 import org.netlib.util.intW;
 
 import java.text.DecimalFormat;
@@ -11,32 +10,34 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.IntStream;
 
+/**
+ * Principal component analysis (PCA) is a statistical procedure that uses an orthogonal transformation to convert a set
+ * of observations of possibly correlated variables (entities each of which takes on various numerical values) into a
+ * set of values of linearly uncorrelated variables called principal components.
+ */
 public class PCA {
 
-    private final static Logger logger = Logger.getLogger(PCA.class);
-
+    /*
     public static void main(String[] args) {
 
-        double[] vectors = new double[] {4, 6, 10, 3, 10, 13, -2, -6, -8, 1, 7, 5};
+        double[] vectors = new double[] {0.709021285176277, 0.0989727054256946, 0.944028885336593, 0.131136209238321, 0.627006936818361, 0.284031824674457, 0.299683759687468, 0.704704837407917, 0.316494380356744, 0.316406882135198, 0.538645294960588, 0.180753013351932, 0.881154121831059, 0.0677755326032639, 0.391978225670755, 0.0379904755391181, 0.987172610359266, 0.762468789936975, 0.484379547648132, 0.787211643299088, 0.23481377819553, 0.098848425084725, 0.970515887485817, 0.147545246174559, 0.176420934265479, 0.529135998338461, 0.911328493384644, 0.979128144215792, 0.809106114786118, 0.960821788525209, 0.284524141578004, 0.360719811171293, 0.627841244451702, 0.230615806300193, 0.821070936974138, 0.967762595973909, 0.606781761394814, 0.931929770857096, 0.220125934574753, 0.049749348545447, 0.453613267978653, 0.12004346284084, 0.211779947625473, 0.766710783354938, 0.475467096082866, 0.435741536319256, 0.670769516611472, 0.00508560473099351, 0.319875477347523, 0.54805191908963, 0.548542238073424, 0.945220392663032, 0.543160151224583, 0.509918361436576, 0.107692875666544, 0.813819201430306, 0.76229839771986, 0.19540986744687, 0.493820405565202, 0.459453582298011, 0.50717785791494, 0.828551664017141, 0.411244156537578, 0.441769652301446, 0.956746453652158, 0.474191799294204, 0.920670317718759, 0.572604786138982, 0.73090164992027, 0.15609525074251, 0.694328867364675, 0.205678805941716, 0.550567029742524, 0.330725627951324, 0.503993728896603, 0.163677414646372, 0.625064524356276, 0.873371527297422, 0.839583723573014, 0.189996645087376, 0.650658410508186, 0.453810701379552, 0.236313452012837, 0.912110481178388, 0.326313535915688, 0.333147555589676, 0.524009252665564, 0.234035331057385, 0.991670460673049, 0.585701904492453};
         int dim = 3;
 
-        PCA pca = new PCA(vectors, dim, false);
+        PCA pca = new PCA(vectors, dim);
+        System.out.println(pca);
+        Projection projection = pca.project(0.5);
+        System.out.println("Projection:\n" + projection);
 
-    }
+    }*/
 
-    private String toString(double[] data, int nCols) {
+    private static String toStringMatrix(double[] data, int nCols) {
 
-        StringBuilder out = new StringBuilder();
+        StringBuilder out = new StringBuilder(data.length * 7 + nCols * 2);
         DecimalFormat df = new DecimalFormat("####0.0000");
-        final int nRows = data.length / nCols;
-        for (int i = 0; i < nRows; i++) {
-            for (int j = 0; j < nCols; j++) {
-                double value = data[i + j * nRows];
-                if (value >= 0)
-                    out.append(" ");
-                out.append(" " + df.format(value));
-            }
-            out.append("\n");
+
+        for(int i = 0; i < data.length; i ++) {
+            out.append(" ").append(df.format(data[i]));
+            if((i % nCols) == nCols-1) out.append("\n");
         }
 
         return out.toString();
@@ -45,12 +46,32 @@ public class PCA {
     /**
      * Contains the real and imaginary parts of the eigenvalues
      */
-    private final double[] eigenValuesReal, eigenValuesImaginary, varPercentage, sd;
-    private final double[] data, work, covarianceMatrix, leftEigenVectors;
+    private final double[] eigenValuesReal;
+    private final double[] varPercentage;
+    private final double[] sd;
+    private final double[] data;
+    private final double[] covarianceMatrix;
+    private final double[] leftEigenVectors;
     private final int nRows, nCols;
-    int[] sortedIndices;
+    private int[] sortedIndices;
 
-    public PCA(double[] vectors, int dim, boolean colMajor)  {
+    /**
+     * Calculate all necessary prerequisites for projecting the input matrix into a lower dimensional space. Will create
+     * a new matrix and not overwrite the input matrix.
+     * @param vectors Input matrix
+     * @param dim Number of columns of the input matrix
+     */
+    public PCA(double[] vectors, int dim) {
+        this(vectors, dim, false);
+    }
+
+    /**
+     * Calculate all necessary prerequisites for projecting the input matrix into a lower dimensional space
+     * @param vectors Input matrix
+     * @param dim Number of columns of the input matrix
+     * @param inPlace Whether to overwrite the input matrix (true) or create a copy (false)
+     */
+    public PCA(double[] vectors, int dim, boolean inPlace)  {
 
         assert vectors.length != 0;
         assert vectors.length % dim == 0;
@@ -59,21 +80,19 @@ public class PCA {
         this.nRows = vectors.length / dim;
 
         // Allocate space for the decomposition
-        eigenValuesReal = new double[nRows];
-        eigenValuesImaginary = new double[nRows];
-        sd = new double[nRows];
-        varPercentage = new double[nRows];
+        eigenValuesReal = new double[nCols];
+        double[] eigenValuesImaginary = new double[nCols];
+        sd = new double[nCols];
+        varPercentage = new double[nCols];
         leftEigenVectors = new double[nCols * nCols];
 
-        this.data = Arrays.copyOf(vectors, vectors.length);
-        if(!colMajor)
-            toColumnMajorInPlace();
+        if(inPlace)
+            this.data = vectors;
+        else
+            this.data = Arrays.copyOf(vectors, vectors.length);
 
-        System.out.println(toString(data, dim));
-        center();
-        System.out.println(toString(data, dim));
-        covarianceMatrix = covariance();
-        System.out.println(toString(covarianceMatrix, dim));
+        centerRowMajor();
+        covarianceMatrix = covarianceRowMajor();
 
         // Find the needed workspace
         double[] workSize = new double[1];
@@ -85,14 +104,14 @@ public class PCA {
                 workSize, -1, info);
 
         // Allocate workspace
-        int workSpace = 0;
+        int workSpace;
         if (info.val != 0) {
             workSpace = 4 * nRows;
         } else
             workSpace = (int) workSize[0];
 
         workSpace = Math.max(1, workSpace);
-        work = new double[workSpace];
+        double[] work = new double[workSpace];
 
         info = new intW(0);
         ld = FastMath.max(1, nCols);
@@ -106,83 +125,89 @@ public class PCA {
                 .mapToInt(i -> i).toArray();
 
         double sum = 0;
-        for(int i = 0; i < eigenValuesReal.length; i++) sum += eigenValuesReal[i];
+        for (double v : eigenValuesReal) sum += v;
 
         for(int i = 0; i < eigenValuesReal.length; i++) {
             varPercentage[i] = FastMath.abs(eigenValuesReal[i] / sum);
             sd[i] = FastMath.sqrt(FastMath.abs(eigenValuesReal[i]));
         }
 
-        System.out.println("eigen values:\n"+toString(eigenValuesReal, dim));
-        System.out.println("variance as percentage:\n"+toString(varPercentage, dim));
-        System.out.println("standard deviation:\n"+toString(sd, dim));
-        System.out.println("eigen vectors:\n"+toString(leftEigenVectors, dim));
-        System.out.println("projection:\n"+toString(project(), dim));
     }
 
-    private double[] project() {
+    @Override
+    public String toString() {
 
-        double[] projection = new double[data.length];
-        int nRowsData = data.length / nCols;
-        int nColsEigenVectors = nCols;
-
-        for (int r = 0; r < nRowsData; r++) {
-            for (int c = 0; c < nColsEigenVectors; c++) {
-                for (int k = 0; k < nColsEigenVectors; k++) {
-                    projection[r + c * nRowsData] += data[r + k * nRowsData] * leftEigenVectors[k + sortedIndices[c] * nColsEigenVectors];
-                }
-            }
-        }
-        return  projection;
-    }
-
-    private void toColumnMajorInPlace() {
-        final int mn1 = data.length - 1;
-        final boolean[] visited = new boolean[data.length];
-        int c = 0;
-        while (++c != data.length) {
-            if (visited[c]) continue;
-            int a = c;
-            do {
-                a = a == mn1 ? mn1 : (nRows * a) % mn1;
-                swap(data, a, c);
-                visited[a] = true;
-            } while (a != c);
-        }
+        return "PCA:\n" +
+                "\nCovariance matrix:\n" + toStringMatrix(covarianceMatrix, nCols) +
+                "\nVariance as percentage:\n" + toStringMatrix(varPercentage, nCols) +
+                "\nStandard deviation:\n" + toStringMatrix(sd, nCols) +
+                "\nEigen-values:\n" + toStringMatrix(eigenValuesReal, nCols) +
+                "\nEigen-vectors:\n" + toStringMatrix(leftEigenVectors, nCols);
     }
 
     /**
-     * Swaps the values of two indices in the given array
-     * @param d The array to swap values in
-     * @param a The first index
-     * @param b The second index
+     * Project the data using the pre-calculated eigenvectors
+     * @param minVariance Used to determine the number of principal components that have to be used
+     * @return The projected matrix
      */
-    private void swap(double[] d, int a, int b) {
-        double t = d[a];
-        d[a] = d[b];
-        d[b] = t;
+    public Projection project(double minVariance) {
+
+        int maxEigenCols = getCumulativeVarianceIndex(minVariance);
+
+        double[] projection = new double[nRows * maxEigenCols];
+        int nRowsData = data.length / nCols;
+        int nColsEigenVectors = nCols;
+
+        // Note that LAPACK returns the eigenvectors in transposed fashion
+        // So we have to keep that in mind while indexing
+
+        for (int r = 0; r < nRowsData; r++) {
+            for (int c = 0; c < maxEigenCols; c++) {
+                for (int k = 0; k < nColsEigenVectors; k++) {
+                    projection[c + r * maxEigenCols] +=
+                            data[k + r * nCols] * leftEigenVectors[k + sortedIndices[c] * nColsEigenVectors];
+                }
+            }
+        }
+
+        return new Projection(projection, maxEigenCols);
+    }
+
+    /**
+     * Calculate the number of columns (principal components) we need to include in the projection
+     * in order to explain at least the minimum variance given as the argument
+     * @param minVariance The minimum variance
+     * @return A number ranging from 1 and the number of eigenvalues + 1
+     */
+    private int getCumulativeVarianceIndex(double minVariance) {
+        double cumulativeVariance = 0;
+        int i = 0;
+        while(cumulativeVariance < minVariance) {
+            cumulativeVariance += varPercentage[sortedIndices[i++]];
+        }
+        return i;
     }
 
     /**
      * Calculate the mean of every column
      * @return An array of means
      */
-    private double[] columnMeans() {
+    private double[] columnMeansRowMajor() {
         double[] means = new double[nCols];
         for(int i = 0; i < data.length; i++) {
-            int row = i % nRows, col = i / nRows;
+            int row = i / nCols, col = i % nCols;
             means[col] = (data[i] + row * means[col]) / (row + 1);
         }
         return means;
     }
 
     /**
-     * Center the data around 0, will replace the input matrix with new values
+     * Center the data around 0, will replace the data matrix with new values
      */
-    private void center() {
-        double[] means = columnMeans();
+    private void centerRowMajor() {
+        double[] means = columnMeansRowMajor();
         for(int i = 0; i < data.length; i++) {
-            data[i] -= means[i / nRows];
+            data[i] -= means[i % nCols];
         }
     }
 
@@ -190,21 +215,19 @@ public class PCA {
      * Calculate the covariance of an input matrix
      * @return A new square matrix of size nCols*nCols
      */
-    private double[] covariance() {
+    private double[] covarianceRowMajor() {
 
         double[] covMatrix = new double[nCols*nCols];
 
-        /*
-        Note that at this point the data has been centered, which means that
-        we do not have to subtract the column means as they are all 0
-         */
+        // Note that at this point the data has been centered, which means that
+        // we do not have to subtract the column means (as they are all 0)
 
         for(int col1 = 0; col1 < nCols; col1++) {
             for(int col2 = 0; col2 < nCols; col2++) {
 
                 double cov = 0;
                 for(int i = 0; i < nRows; i++) {
-                    cov += (data[i + col1 * nRows] ) * (data[i + col2 * nRows] );
+                    cov += data[i * nCols + col1] * data[i * nCols + col2] ;
                 }
                 cov /= (nRows - 1);
                 covMatrix[col1 + col2 * nCols] = cov;
@@ -213,4 +236,27 @@ public class PCA {
         return covMatrix;
     }
 
+    public class Projection {
+
+        final double[] projection;
+        final int nCols;
+
+        public Projection(double[] projection, int nCols) {
+            this.projection = projection;
+            this.nCols = nCols;
+        }
+
+        @Override
+        public String toString() {
+            return PCA.toStringMatrix(projection, nCols);
+        }
+
+        public double[] getProjection() {
+            return projection;
+        }
+
+        public int getnCols() {
+            return nCols;
+        }
+    }
 }
