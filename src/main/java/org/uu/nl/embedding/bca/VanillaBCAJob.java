d8580bd490f5f565ac57aa1f533224a1a91feb51
Jurian <j.baas@uu.nl>
2019-03-29 17:15:26 +0100

    Fully removed use of precomputed BCV's

--- src/main/java/org/uu/nl/embedding/bca/VanillaBCAJob.java
+++ src/main/java/org/uu/nl/embedding/bca/VanillaBCAJob.java
@@ -6,33 +6,18 @@ import org.uu.nl.embedding.bca.util.BCV;
 import org.uu.nl.embedding.bca.util.PaintRegistry;
 
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 public class VanillaBCAJob extends BCAJob {
 
-	private final Map<Integer, BCV> computedBCV;
 	private final int[][] out, in;
 	
-	public VanillaBCAJob(Grph graph, Map<Integer, BCV> computedBCV, 
+	public VanillaBCAJob(Grph graph,
 			int bookmark, boolean reverse, double alpha, double epsilon,
 			int[][] in, int[][] out) {
 		super(bookmark, reverse, alpha, epsilon, graph);
-		this.computedBCV = computedBCV;
 		this.out = out;
 		this.in = in;
-	}
-
-	public String nodeLabel(int n) {
-		return graph.getVertexLabelProperty().getValueAsString(n);
-	}
-	
-	private int getEdgeType(int e) {
-		return graph.getEdgeColorProperty().getValueAsInt(e);
-	}
-
-	private String edgeLabel(int e) {
-		return graph.getEdgeLabelProperty().getValueAsString(e);
 	}
 
 	@Override
@@ -48,37 +33,31 @@ public class VanillaBCAJob extends BCAJob {
 		int[] neighbors, edgeCache;
 		int focusNode, predicate;
 		double partialWetPaint;
-		BCV precomputed;
+
 		
 		while (!nodeQueue.isEmpty()) {
 
 			focusNode = nodeQueue.poll();
 			final double wetPaint = wetPaintRegister.get(focusNode);
 
-			//precomputed = computedBCV.get(focusNode);
-			if(false){//precomputed != null) {
-				precomputed.forEach((index, precomputedPaint) -> {
-					final double scaled = precomputedPaint * wetPaint;
-					if(scaled > (epsilon * alpha)) bcv.add(index, scaled);
-				});
-			} else {
-				// Keep part of the available paint on this node, distribute the rest
-				bcv.add(focusNode, (alpha * wetPaint));
 
-				// If there is not enough paint we stop and don't distribute among the neighbors
-				if (wetPaint < epsilon)
-					continue;
+            // Keep part of the available paint on this node, distribute the rest
+            bcv.add(focusNode, (alpha * wetPaint));
 
-				if(reverse) neighbors = in[focusNode];
-				else neighbors = out[focusNode];
+            // If there is not enough paint we stop and don't distribute among the neighbors
+            if (wetPaint < epsilon)
+                continue;
 
-				if(neighbors.length == 0)
-					continue;
+            if(reverse) neighbors = in[focusNode];
+            else neighbors = out[focusNode];
 
-				if(reverse) edgeCache = graph.getInOnlyEdges(focusNode).toIntArray();
-				else edgeCache = graph.getOutOnlyEdges(focusNode).toIntArray();
+            if(neighbors.length == 0)
+                continue;
 
-				partialWetPaint = (1 - alpha) * wetPaint / neighbors.length;
+            if(reverse) edgeCache = graph.getInOnlyEdges(focusNode).toIntArray();
+            else edgeCache = graph.getOutOnlyEdges(focusNode).toIntArray();
+
+            partialWetPaint = (1 - alpha) * wetPaint / neighbors.length;
 
 				/*
 				System.out.println(
@@ -90,28 +69,28 @@ public class VanillaBCAJob extends BCAJob {
 								" partial wet paint:" + partialWetPaint);
 				*/
 
-				// We can already tell that the neighbors will not have enough paint to continue
-				if(partialWetPaint < epsilon)
-					continue;
+            // We can already tell that the neighbors will not have enough paint to continue
+            if(partialWetPaint < epsilon)
+                continue;
 
-				for (int neighbor : neighbors) {
+            for (int neighbor : neighbors) {
 
-					if (reverse)
-						predicate = getEdge(neighbor, focusNode, graph.getOutOnlyEdges(neighbor).toIntArray(), edgeCache);
-					else
-						predicate = getEdge(focusNode, neighbor, edgeCache, graph.getInOnlyEdges(neighbor).toIntArray());
+                if (reverse)
+                    predicate = getEdge(neighbor, focusNode, graph.getOutOnlyEdges(neighbor).toIntArray(), edgeCache);
+                else
+                    predicate = getEdge(focusNode, neighbor, edgeCache, graph.getInOnlyEdges(neighbor).toIntArray());
 
-					//System.out.println(nodeLabel(bookmark) + ": " + edgeLabel(predicate) + " -> " + getEdgeType(predicate));
-					bcv.add(graph.getVertices().size() + getEdgeType(predicate), partialWetPaint);
+                //System.out.println(nodeLabel(bookmark) + ": " + edgeLabel(predicate) + " -> " + getEdgeType(predicate));
+                bcv.add(graph.getVertices().size() + getEdgeType(predicate), partialWetPaint);
 
-					if (nodeQueue.contains(neighbor)) {
-						wetPaintRegister.add(neighbor, partialWetPaint);
-					} else {
-						nodeQueue.add(neighbor);
-						wetPaintRegister.put(neighbor, partialWetPaint);
-					}
-				}
-			}
+                if (nodeQueue.contains(neighbor)) {
+                    wetPaintRegister.add(neighbor, partialWetPaint);
+                } else {
+                    nodeQueue.add(neighbor);
+                    wetPaintRegister.put(neighbor, partialWetPaint);
+                }
+            }
+
 
 			//if(debug) System.out.println(nodeLabel(bookmark) + ": " + bcv);
 		}
