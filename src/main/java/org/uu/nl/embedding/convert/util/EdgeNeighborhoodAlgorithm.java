51c2260dc577af3e9af2e6889a8798322b28c82f
Jurian <j.baas@uu.nl>
2019-04-08 13:56:38 +0200

    Constructing the edge neighborhoods is now done in parallel

--- src/main/java/org/uu/nl/embedding/convert/util/EdgeNeighborhoodAlgorithm.java
+++ src/main/java/org/uu/nl/embedding/convert/util/EdgeNeighborhoodAlgorithm.java
@@ -9,58 +9,14 @@ import toools.collections.primitive.IntCursor;
 import toools.collections.primitive.LucIntSet;
 
 import java.util.Iterator;
+import java.util.concurrent.*;
 
+@SuppressWarnings("Duplicates")
 public abstract class EdgeNeighborhoodAlgorithm extends VertexAdjacencyAlgorithm {
 
     private static final Settings settings = Settings.getInstance();
 
-    @Override
-    public int[][] compute(Grph g) {
-
-        LucIntSet vertices = g.getVertices();
-
-        if (vertices.isEmpty()) return new int[0][];
-
-        final int n = vertices.getGreatest() + 1;
-        final int[][] v = new int[n][];
-
-        final Iterator iterator = IntCursor.fromFastUtil(vertices).iterator();
-
-        IntCursor c;
-        switch (getDirection()){
-            case in:
-                try(ProgressBar bp = settings.progressBar("Edge in", n, "nodes")) {
-                    while(iterator.hasNext()) {
-                        c = (IntCursor) iterator.next();
-                        int[] neighbors = g.getNeighbours(c.value, Grph.DIRECTION.in).toIntArray();
-                        LucIntSet edges = g.getInEdges(c.value);
-                        v[c.value] = new int[neighbors.length];
-                        for(int i = 0; i < neighbors.length; i++)
-                            v[c.value][i] = getEdge(g, neighbors[i], c.value, g.getOutEdges(neighbors[i]), edges);
-                        bp.step();
-                    }
-                }
-
-                break;
-            case out:
-                try(ProgressBar bp = settings.progressBar("Edge out", n, "nodes")) {
-                    while (iterator.hasNext()) {
-                        c = (IntCursor) iterator.next();
-                        int[] neighbors = g.getNeighbours(c.value, Grph.DIRECTION.out).toIntArray();
-                        LucIntSet edges = g.getOutEdges(c.value);
-                        v[c.value] = new int[neighbors.length];
-                        for (int i = 0; i < neighbors.length; i++)
-                            v[c.value][i] = getEdge(g, c.value, neighbors[i], edges, g.getInEdges(neighbors[i]));
-                        bp.step();
-                    }
-                }
-                break;
-        }
-
-        return v;
-    }
-
-    private int getEdge(Grph graph, int source, int destination, LucIntSet out, LucIntSet in) {
+    private static int getEdge(Grph graph, int source, int destination, LucIntSet out, LucIntSet in) {
 
         if (out.size() == 0 || in.size() == 0) return -1;
 
@@ -80,12 +36,130 @@ public abstract class EdgeNeighborhoodAlgorithm extends VertexAdjacencyAlgorithm
         public Grph.DIRECTION getDirection() {
             return Grph.DIRECTION.in;
         }
+
+        @Override
+        public int[][] compute(Grph g) {
+
+            final int[] vertices = g.getVertices().toIntArray();
+
+            if (vertices.length == 0) return new int[0][];
+
+            final int numThreads = settings.threads();
+            final int numVertices = vertices.length;
+            final int[][] v = new int[numVertices][];
+            final int[] verticesPerThread = new int[numThreads];
+            for(int i = 0; i < numThreads; i++) verticesPerThread[i] = numVertices / numThreads;
+            verticesPerThread[numThreads - 1] += numVertices % numThreads;
+            final ExecutorService es = Executors.newWorkStealingPool(numThreads);
+            final CompletionService<Void> cs = new ExecutorCompletionService<>(es);
+
+            try(ProgressBar bp = settings.progressBar("Edge in", numVertices, "nodes")) {
+
+                for(int t = 0; t < numThreads; t++) {
+                    final int threadId = t;
+                    cs.submit(() -> {
+
+                        int c;
+                        final int offset = (numVertices / numThreads) * threadId;
+                        int[] neighbors;
+                        LucIntSet edgesUnordered;
+
+                        for(int i = 0; i < verticesPerThread[threadId]; i++) {
+                            c = vertices[offset + i];
+                            neighbors = g.getNeighbours(c, Grph.DIRECTION.in).toIntArray();
+                            edgesUnordered = g.getInEdges(c);
+                            v[c] = new int[neighbors.length];
+                            for(int n = 0; n < neighbors.length; n++)
+                                v[c][n] = getEdge(g, neighbors[n], c, g.getOutEdges(neighbors[n]), edgesUnordered);
+                            bp.step();
+
+                        }
+                        return null;
+                    });
+                }
+
+                int handled = 0;
+                while(handled < numThreads) {
+                    try {
+                        cs.take().get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    } finally {
+                        handled++;
+                    }
+                }
+
+
+            }
+
+            return v;
+        }
+
+
     }
 
     public static class Out extends EdgeNeighborhoodAlgorithm {
 
         public Grph.DIRECTION getDirection() {
             return Grph.DIRECTION.out;
+        }
+
+        @Override
+        public int[][] compute(Grph g) {
+
+            final int[] vertices = g.getVertices().toIntArray();
+
+            if (vertices.length == 0) return new int[0][];
+
+            final int numThreads = settings.threads();
+            final int numVertices = vertices.length;
+            final int[][] v = new int[numVertices][];
+            final int[] verticesPerThread = new int[numThreads];
+            for(int i = 0; i < numThreads; i++) verticesPerThread[i] = numVertices / numThreads;
+            verticesPerThread[numThreads - 1] += numVertices % numThreads;
+            final ExecutorService es = Executors.newWorkStealingPool(numThreads);
+            final CompletionService<Void> cs = new ExecutorCompletionService<>(es);
+
+            try(ProgressBar bp = settings.progressBar("Edge in", numVertices, "nodes")) {
+
+                for(int t = 0; t < numThreads; t++) {
+                    final int threadId = t;
+                    cs.submit(() -> {
+
+                        int c;
+                        final int offset = (numVertices / numThreads) * threadId;
+                        int[] neighbors;
+                        LucIntSet edgesUnordered;
+
+                        for(int i = 0; i < verticesPerThread[threadId]; i++) {
+                            c = vertices[offset + i];
+                            neighbors = g.getNeighbours(c, Grph.DIRECTION.out).toIntArray();
+                            edgesUnordered = g.getOutEdges(c);
+                            v[c] = new int[neighbors.length];
+                            for(int n = 0; n < neighbors.length; n++)
+                                v[c][n] = getEdge(g, c, neighbors[n], edgesUnordered, g.getInEdges(neighbors[n]));
+                            bp.step();
+
+                        }
+                        return null;
+                    });
+                }
+
+                int handled = 0;
+                while(handled < numThreads) {
+                    try {
+                        cs.take().get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    } finally {
+                        handled++;
+                    }
+                }
+
+
+            }
+
+            return v;
         }
     }
 }
