be683b0d5c7d87768375f5d73c1e23166bf8f225
Jurian <j.baas@uu.nl>
2019-04-16 17:30:17 +0200

    Comments, spaces and empty lines are now supported and errors are thrown when parts cannot be parsed

--- src/main/java/org/uu/nl/embedding/util/read/WeightsReader.java
+++ src/main/java/org/uu/nl/embedding/util/read/WeightsReader.java
@@ -1,10 +1,14 @@
 package org.uu.nl.embedding.util.read;
 
+import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WeightsReader implements Reader<Map<String, Integer>> {
+
+    private static final Logger logger = Logger.getLogger(WeightsReader.class);
 
     @Override
     public Map<String, Integer> load(File file) throws IOException {
@@ -17,10 +21,30 @@ public class WeightsReader implements Reader<Map<String, Integer>> {
 
             String line;
             while((line = br.readLine()) != null) {
-                map.putIfAbsent(
-                        line.substring(0, line.indexOf('=')),
-                        Integer.parseInt(line.substring(line.indexOf('=')+1))
-                );
+
+                line = line.replace(" ", "");
+                if(line.length() == 0) continue; // Skip empty lines
+
+                final int commentPos = line.indexOf('#');
+
+                if(commentPos == 0) continue; // Whole line is comment
+                if(commentPos >= 0) line = line.substring(0, commentPos); // Remove comment part
+
+
+                try {
+
+                    final int equals = line.indexOf('=');
+                    final String key = line.substring(0, equals);
+                    final String value = line.substring(equals + 1);
+
+                    try {
+                        map.putIfAbsent(key, Integer.parseInt(value));
+                    } catch (NumberFormatException e) {
+                        logger.error("Unable to parse integer " + value);
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    logger.error("Unable to parse line " + line);
+                }
             }
         }
 
