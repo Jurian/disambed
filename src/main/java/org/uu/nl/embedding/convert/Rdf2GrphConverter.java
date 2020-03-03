e3d289b58c481cb349b1fd69fb1b783f34ff128f
Jurian <j.baas@uu.nl>
2020-03-03 13:15:46 +0100

    Added documentation and streamlined code

--- src/main/java/org/uu/nl/embedding/convert/Rdf2GrphConverter.java
+++ src/main/java/org/uu/nl/embedding/convert/Rdf2GrphConverter.java
@@ -2,7 +2,6 @@ package org.uu.nl.embedding.convert;
 
 import grph.Grph;
 import grph.in_memory.InMemoryGrph;
-import grph.properties.NumericalProperty;
 import grph.properties.Property;
 import me.tongfei.progressbar.ProgressBar;
 import org.apache.jena.graph.Node;
@@ -29,14 +28,14 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 
 	private static final Logger logger = Logger.getLogger(Rdf2GrphConverter.class);
 
-	private final Map<String, Double> weights;
+	private final Map<String, Double> predicateWeights;
 	private final Map<String, SimilarityGroup<String>> similarityGroups;
 	private final Configuration config;
 
 	public Rdf2GrphConverter(Configuration config) {
 
 		this.config = config;
-		this.weights = config.getWeights();
+		this.predicateWeights = config.getWeights();
 		this.similarityGroups = new HashMap<>();
 		config.getSimilarity().forEach(
 				sim -> similarityGroups.put(
@@ -58,14 +57,14 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 		final Grph g = new InMemoryGrph();
 
 		final Map<Node, Integer> vertexMap = new HashMap<>();
-		final Map<Node, Integer> edgeMap = new HashMap<>();
+		final Map<Node, Integer> edgeTypes = new HashMap<>();
 
 		long skippedTriples = 0;
-		int s_i, o_i, p_i, edgeType;
+		int s_i, o_i;
 		final boolean doSimilarityMatching = !similarityGroups.isEmpty();
 		String predicateString;
 		Node s, p, o;
-		Triple t   ;
+		Triple t ;
 
 		final ExtendedIterator<Triple> triples = model.getGraph().find();
 
@@ -80,7 +79,7 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 				predicateString = p.toString();
 
 				// Ignore unweighted predicates
-				if(!weights.containsKey(predicateString)) {
+				if(!predicateWeights.containsKey(predicateString)) {
 					// Adjust the total number of triples we are considering
 					// Maybe we can do pb.step() here instead to make it less confusing
 					pb.step();
@@ -88,36 +87,22 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 					continue;
 				}
 
-				// Only create a new ID if the subject is not yet present
-				if (!vertexMap.containsKey(s)) addVertex(g, s, vertexMap);
-				if (!vertexMap.containsKey(o)) addVertex(g, o, vertexMap);
+				// Only create a new ID if the vertex is not yet present
+				s_i = addVertex(g, s, vertexMap);
+				o_i = addVertex(g, o, vertexMap);
+				addEdge(g, p, s_i, o_i, edgeTypes);
 
-				s_i = vertexMap.get(s);
-				o_i = vertexMap.get(o);
-
-				if(doSimilarityMatching && similarityGroups.containsKey(predicateString)) {
-					similarityGroups.get(predicateString).preprocess(o.getLiteralValue().toString(), o_i);
+				if(doSimilarityMatching) {
+					// Some similarity metrics require pre-processing
+					final SimilarityGroup<String> sg = similarityGroups.get(predicateString);
+					if(sg != null && sg.similarity.needsPreproces()) sg.preprocess(o.getLiteralValue().toString(), o_i);
 				}
-
-				// Every edge is unique, we always add one to the graph
-				// However there are only a few types of edges (relationships)
-				// So we also store a reference to a unique edge-type id
-				p_i = g.addDirectedSimpleEdge(s_i, o_i);
-
-				g.getEdgeLabelProperty().setValue(p_i, predicateString);
-
-				// If we have not encountered this edge-type before, give it a unique id
-				edgeMap.putIfAbsent(p, edgeMap.size());
-				// Store the edge-type value for this new edge
-				edgeType = edgeMap.get(p);
-
-				g.getEdgeColorProperty().setValue(p_i, edgeType);
 
 				pb.step();
 			}
 		} finally {
 			triples.close();
-			if(skippedTriples > 0) logger.info("Skipped " + skippedTriples +
+			logger.info("Skipped " + skippedTriples +
 					" unweighted triples (" + String.format("%.2f", (skippedTriples/(double)model.size()*100)) + " %)");
 		}
 
@@ -155,6 +140,8 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 								final int e1 = g.addDirectedSimpleEdge(vert, otherVert);
 								final int e2 = g.addDirectedSimpleEdge(otherVert, vert);
 
+								// Similarities between vertices are stored as edge width property, which is limited to
+								// a few bits so we store the similarity as a byte (a number between 0 and 100)
 								g.getEdgeWidthProperty().setValue(e1, similarity);
 								g.getEdgeWidthProperty().setValue(e2, similarity);
 
@@ -181,6 +168,9 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 		return g;
 	}
 
+	/**
+	 * Instantiate a similarity object from the configuration information
+	 */
 	private Similarity<String> createSimilarityMetric(Configuration.SimilarityGroup sim ) {
 
 		switch (sim.getMethodEnum()) {
@@ -197,14 +187,42 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 		return null;
 	}
 
-	private void addVertex(Grph g, Node n, Map<Node, Integer> vertexMap) {
+	/**
+	 * Add a vertex to the graph if not present, the vertices are given consecutive IDs.
+	 * Node type and label information are also added
+	 */
+	private int addVertex(Grph g, Node n, Map<Node, Integer> vertexMap) {
+		final Integer key = vertexMap.get(n);
+		if(key != null) return key; // Vertex already present
+
 		final int i = vertexMap.size();
-		g.addVertex(i);
-		g.getVertexColorProperty().setValue(i, type2color(n));
-		g.getVertexLabelProperty().setValue(i, n.toString(false));
 		vertexMap.put(n, i);
+		g.addVertex(i);
+		// Add vertex type info (URI, blank, literal)
+		g.getVertexColorProperty().setValue(i, type2color(n));
+		// Add vertex label information (used for the embedding dictionary)
+		g.getVertexLabelProperty().setValue(i, n.toString(false));
+		return i;
     }
 
+	/**
+	 * 	Edges are considered unique for a subject-object pair
+	 * 	However there are only a few types of edges (relationships)
+	 * 	So we also store a reference to a unique edge-type id
+	 */
+    private void addEdge(Grph g, Node p, int s_i, int o_i, Map<Node, Integer> edgeTypes ) {
+    	// Create a unique id for this predicate given the subject-object pair
+		final int p_i = g.addDirectedSimpleEdge(s_i, o_i);
+		g.getEdgeLabelProperty().setValue(p_i, p.toString(false));
+		// If we have not encountered this edge-type before, give it a unique id
+		edgeTypes.putIfAbsent(p, edgeTypes.size());
+		// Store the edge-type value for this new edge
+		g.getEdgeColorProperty().setValue(p_i, edgeTypes.get(p));
+	}
+
+	/**
+	 * Associates a set of nodes with a similarity metric
+	 */
 	private static class SimilarityGroup<T> {
 
 		public final Similarity<T> similarity;
@@ -223,6 +241,9 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 		}
 	}
 
+	/**
+	 * The result of comparing a node with other nodes
+	 */
 	private static class CompareResult {
 
 		public final int vert;
@@ -236,6 +257,9 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 		}
 	}
 
+	/**
+	 * Do the similarity matching in parallel
+	 */
 	private static class CompareJob implements Callable<CompareResult> {
 
 		private final int index;
@@ -266,7 +290,7 @@ public class Rdf2GrphConverter implements Converter<Model, Grph> {
 				final double similarity = metric.calculate(s1, s2);
 
 				if (similarity >= threshold) {
-
+					// For reasons mentioned above, we store similarities as a byte
 					final byte b = (byte) (similarity * 100);
 					result.otherVerts.add(otherVert);
 					result.similarities.add(b);
