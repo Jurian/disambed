306c4ec0ed3211c93d82c57638dad147b7871a94
Euan <euanw93@gmail.com>
2020-05-25 15:41:50 +0200

    Slight change for static use of class in contextAwareBcv

--- src/main/java/org/uu/nl/embedding/logic/ExactSameDateLogic.java
+++ src/main/java/org/uu/nl/embedding/logic/ExactSameDateLogic.java
@@ -11,23 +11,37 @@ import org.uu.nl.embedding.logic.util.SimpleDate;
  * 		two dates should be exactly the same.
  * 
  * @author Euan Westenbroek
- * @version 1.0
+ * @version 1.1
  * @since 13-05-2020
  */
-public class SameDateLogic extends DateCompareLogic {
+public class ExactSameDateLogic extends DateCompareLogic {
 	
 	protected DateLogic firstDay;
 	protected DateLogic secondDay;
 	protected boolean isDate;
 	
 	/**
+	 * Constructor method for undefined class
+	 * WARNING: Solely use static methods!
+	 */
+	public ExactSameDateLogic() {
+		/*
+		 * This is a filler constructor to use for
+		 * creating an undefined class initialization
+		 * which makes it possible to use this class
+		 * as a logic type only to use its static methods.
+		 */ 
+	}
+	
+	/**
 	 * Constructor method without user-given name declaration.
 	 * 
-	 * @param term A LogicRule class representing the logic date format 
+	 * @param firstDate A String representing the logic date format 
+	 * @param secondDate A String representing the logic date format
 	 */
-	public SameDateLogic(String firstDate, String secondDate) {
+	public ExactSameDateLogic(String firstDate, String secondDate) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 		
 		this.name = ("isSameDate(" + this.firstDay.getName() + ", " + this.secondDay.getName() + ")");
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -36,12 +50,13 @@ public class SameDateLogic extends DateCompareLogic {
 	/**
 	 * Constructor method without user-given name declaration.
 	 * 
-	 * @param term A LogicRule class representing the logic date format 
+	 * @param firstDate A String representing the logic date format 
+	 * @param secondDate A String representing the logic date format
 	 * @param name The given name of the logic term defined by the user
 	 */
-	public SameDateLogic(String firstDate, String secondDate, String name) {
+	public ExactSameDateLogic(String firstDate, String secondDate, String name) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 		
 		this.name = name;
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -53,9 +68,9 @@ public class SameDateLogic extends DateCompareLogic {
 	 * @param term A LogicRule class representing the logic date format 
 	 * @param term A LogicTerm class representing the negated logic term
 	 */
-	public SameDateLogic(SimpleDate firstDate, SimpleDate secondDate) {
+	public ExactSameDateLogic(SimpleDate firstDate, SimpleDate secondDate) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 
 		this.name = ("isSameDate(" + this.firstDay.getName() + ", " + this.secondDay.getName() + ")");
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -67,9 +82,9 @@ public class SameDateLogic extends DateCompareLogic {
 	 * @param term A LogicRule class representing the logic date format 
 	 * @param name The given name of the logic term defined by the user
 	 */
-	public SameDateLogic(SimpleDate firstDate, SimpleDate secondDate, String name) {
+	public ExactSameDateLogic(SimpleDate firstDate, SimpleDate secondDate, String name) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 		
 		this.name = name;
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -81,9 +96,9 @@ public class SameDateLogic extends DateCompareLogic {
 	 * @param term A LogicRule class representing the logic date format 
 	 * @param term A LogicTerm class representing the negated logic term
 	 */
-	public SameDateLogic(DateLogic firstDate, DateLogic secondDate) {
+	public ExactSameDateLogic(DateLogic firstDate, DateLogic secondDate) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 
 		this.name = ("isSameDate(" + this.firstDay.getName() + ", " + this.secondDay.getName() + ")");
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -95,9 +110,9 @@ public class SameDateLogic extends DateCompareLogic {
 	 * @param term A LogicRule class representing the logic date format 
 	 * @param name The given name of the logic term defined by the user
 	 */
-	public SameDateLogic(DateLogic firstDate, DateLogic secondDate, String name) {
+	public ExactSameDateLogic(DateLogic firstDate, DateLogic secondDate, String name) {
 		super(firstDate, secondDate);
-		compareDates();
+		compareTheseDates();
 		
 		this.name = name;
 		this.str = ("isSameDate(" + this.firstDay.toString() + ", " + this.secondDay.toString() + ")");
@@ -107,8 +122,28 @@ public class SameDateLogic extends DateCompareLogic {
 	 * Inherited compare method to check if:
 	 * 		The two dates are exactly the same
 	 */
-	protected void compareDates() {
+	@Override
+	protected void compareTheseDates() {
 		this.logicValue = this.firstDay.isSameDateAs(this.secondDay);
 	}
+	
+	/**
+	 * Static method two compare two date Strings
+	 * 
+	 * @param firstDate A String representing the logic date format 
+	 * @param secondDate A String representing the logic date format
+	 * @return 	boolean Returns true if the two Strings are exactly
+	 * 			the same date, else return false
+	 */
+	public static boolean compareTwoDates(String firstDate, String secondDate, int daysDifference) {
+		int ignore = daysDifference;
+
+		DateLogic tempFirstDay = new DateLogic(firstDate);
+		DateLogic tempSecondDay = new DateLogic(secondDate);
+		
+		return tempFirstDay.isSameDateAs(tempSecondDay);
+	}
+	
+	
 	
 }
